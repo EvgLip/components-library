7d28e0ad56c31832376828bf050ca4b0333f8865
evgLip <evggenlip@gmail.com>
2024-10-07 10:58:47 +0300

    скрипт бургер меню перемещен в папку компонента burger-menu

---
+++ components/burger-menu/burger-menu.js
@@ -0,0 +1,32 @@
+/* бургер меню */
+
+'use strict';
+
+const burgMenu = document.querySelector('.burger-menu');
+const burgBtnOpen = document.querySelector('#burger-menu-open');
+const burgBtnClose = document.querySelector('#burger-menu-close');
+
+// открытие/закрытие бургер меню
+if (burgBtnOpen)
+{
+  burgBtnOpen.addEventListener('click', function ()
+  {
+    if (burgMenu)
+    {
+      burgMenu.setAttribute('data-open', '');
+      document.body.classList.add("scroll-lock");
+    }
+  });
+}
+
+if (burgBtnClose)
+{
+  burgBtnClose.addEventListener('click', function ()
+  {
+    if (burgMenu)
+    {
+      burgMenu.removeAttribute('data-open');
+      document.body.classList.remove("scroll-lock");
+    }
+  });
+}
